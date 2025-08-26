4a45a51c9c397baa220735c2eea2e773a09c9969
ShahmunJafri <42565399+ShahmunJafri@users.noreply.github.com>
2025-08-25 18:51:45 -0700

    added more characteristics, need more work on enumerators and how to map them

--- inprintz-database/src/actions/actions.ts
+++ inprintz-database/src/actions/actions.ts
@@ -1,18 +1,29 @@
 "use server";
+import { ProofType } from "@/generated/prisma";
 import prisma from "../lib/db";
 import { redirect } from 'next/navigation';
 
 export async function createOrder(formData: FormData){
+
     await prisma.order.create({
         data: {
-            job_name: formData.get("job name") as string || "unnamed",
-            slug: (formData.get("job name") as string)
+            job_name: formData.get("job_name") as string || "unnamed",
+            slug: (formData.get("job_name") as string)
                 .replace(/\s+/g, "-")
                 .toLowerCase() || "unnamed",
-            client: formData.get("client") as string || "unnamed",
-            contact: formData.get("job name") as string || "unnamed",
-            color_match_info: formData.get("job name") as string || "unnamed",
-        }
+            client: formData.get("client") as string || "null",
+            contact: formData.get("contact") as string || "null",
+            po_number: formData.get("po_number") as string || "null",
+            sales_rep: formData.get("sales_rep") as string || "null",
+
+            proofingIsRequired: formData.get("proofingIsRequired") === "on",
+            proofDueDate: typeof formData.get("proofDueDate") === "string" && formData.get("proofDueDate")
+                        ? new Date(formData.get("proofDueDate") as string) : null,
+            notes: formData.get("notes") as string || "null",
+            proofType: proofType, ///error here
+            color_match_info: formData.get("color_match_info") as string || "null",
+
+         }
     });
     redirect('/');
 }
